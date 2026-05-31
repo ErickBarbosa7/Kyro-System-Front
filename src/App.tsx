b08195bcfd4605e3c16632540e07688d7bf3a113
Erick Barbosa <l.bace040211@itses.edu.mx>
2026-05-30 23:00:49 -0600

    feature: correcion de vista

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { ToastProvider } from './providers/ToastProvider';
 import { Sidebar } from './components/Sidebar';
 import { Navbar } from './components/Navbar';
 import { AppRouter } from './router/AppRouter';
 import './App.css'; 
 
+const LayoutWrapper = () => {
+    const location = useLocation();
+    
+    // 2. Definimos las rutas donde NO queremos ver los menús
+    const rutasSinMenu = ['/login', '/registro', '/']; 
+    const ocultarMenu = rutasSinMenu.includes(location.pathname);
+
+    if (ocultarMenu) {
+        return (
+            <div className="kyro-auth-layout">
+                <AppRouter />
+            </div>
+        );
+    }
+
+    // 4. Si estamos en el sistema (rutas privadas), retornamos el diseño completo con Sidebar y Navbar
+    return (
+        <div className="kyro-app-layout">
+            <Navbar />
+            <div className="kyro-content-wrapper">
+                <Sidebar />
+                <main className="kyro-main-content">
+                    <AppRouter />
+                </main>
+            </div>
+        </div>
+    );
+};
+
 function App() {
     return (
         <BrowserRouter>
             <ToastProvider />
-            
-            {/* Nuevo diseño en columnas (Vertical) */}
-            <div className="kyro-app-layout">
-                
-                {/* 1. Navbar domina toda la parte superior */}
-                <Navbar />
-                
-                {/* 2. El "piso" de abajo donde conviven Sidebar y Dashboard */}
-                <div className="kyro-content-wrapper">
-                    <Sidebar />
-                    
-                    <main className="kyro-main-content">
-                        <AppRouter />
-                    </main>
-                </div>
-
-            </div>
-            
+            {/* Llamamos a nuestro nuevo Wrapper aquí adentro */}
+            <LayoutWrapper />
         </BrowserRouter>
     );
 }
