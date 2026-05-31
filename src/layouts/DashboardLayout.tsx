b272809f1bb6ed0810025b436a17d81f760b92ba
Erick Barbosa <l.bace040211@itses.edu.mx>
2026-05-30 21:41:51 -0600

    resolver conflictos y mover cambios a feature/layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { Sidebar } from '../components/Sidebar';
-import { Navbar } from '../components/Navbar';
+
 import './DashboardLayout.css'; 
 
 export const DashboardLayout = () => {
@@ -8,12 +7,12 @@ export const DashboardLayout = () => {
         <div className="layout-container">
             
             {/* 1. Nuestra nueva pieza del menú */}
-            <Sidebar />
+      
 
             <div className="main-content">
                 
                 {/* 2. Nuestra nueva pieza superior */}
-                <Navbar />
+             
 
                 {/* 3. El espacio dinámico para las pantallas */}
                 <main className="content-area">
